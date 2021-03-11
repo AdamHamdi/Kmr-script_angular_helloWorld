67845fb4c65122d8f4c6cf427c353e53883444f2
Adam Hamdi <prodev.webdesign@gmail.com>
2021-03-11 15:33:58 +0100

    service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { MyCompComponent } from './my-comp/my-comp.component';
 import { ProductComponent } from './product/product.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 const routes: Routes = [
   {path:"",component:MyCompComponent},
   {path:"product",component:ProductComponent},
+  {path:"product/:id",component:ProductComponent},
+  {path:"add-product",component:AddProductComponent},
   {path:'**',component:NotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
- 
+
+
 
 
 exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { MyCompComponent } from './my-comp/my-comp.component';
 import { ProductComponent } from './product/product.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyCompComponent,
     ProductComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
